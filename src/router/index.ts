ecdccfc8bb97cf06dd7f2d78b7ecaebaedfbe2dd
dennyy666 <2751525785@qq.com>
2026-05-08 14:44:04 +0800

    login组件移到外面

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ const router = createRouter({
     {
       path: '/login',
       name: 'Login',
-      component: () => import('../views/login/Login.vue'),
+      component: () => import('../login/Login.vue'),
     },
     {
       path: '/layout',
